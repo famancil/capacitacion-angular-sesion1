5dc2df5f127145a03f1fda73a68fa95d646ac767
famancil <famancil.inf@gmail.com>
2022-04-21 12:10:58 -0400

    demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimerComponenteComponent } from './primer-componente/primer-componente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimerComponenteComponent
   ],
   imports: [
     BrowserModule,
